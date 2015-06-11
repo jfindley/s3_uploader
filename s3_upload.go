333d9b0bc79a98a16bf3df6c6d8f26647487a0aa
James Findley <jamesf@nutmeg.com>
2015-06-11 16:40:47 +0100

    First release.
    Removed multisend for now, as the lib has issues.
    Hardcoded region and filetype for simplicity.

--- s3_upload.go
+++ s3_upload.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	"io"
 	"launchpad.net/goamz/aws"
 	"launchpad.net/goamz/s3"
 	"log"
-	"mime"
 	"os"
 	"runtime"
-	"strings"
 	"sync"
 )
 
@@ -21,81 +19,38 @@ const ChunkSize = 1e+7
 
 type Target struct {
 	bucket *s3.Bucket
-	path   string
 	acl    s3.ACL
 }
 
-func sendFiles(target Target, ch chan os.FileInfo) {
+func sendFiles(target Target, ch chan os.FileInfo, dir string) {
 	for fi := range ch {
 		// Recursive operation not supported
 		if fi.IsDir() {
 			continue
 		}
 
-		// Use multisend for large files
-		if fi.Size() > MultipartThreshold {
-			multiSend(target, fi)
-		} else {
-			singleSend(target, fi)
+		file, err := os.Open(dir + "/" + fi.Name())
+		if err != nil {
+			log.Println(err)
+			return
 		}
+
+		buf := bufio.NewReaderSize(file, 16384)
+
+		err = target.bucket.PutReader(
+			fi.Name(),
+			buf,
+			fi.Size(),
+			"binary/octet-stream",
+			target.acl)
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("Completed", fi.Name())
+		return
 	}
-}
-
-func singleSend(target Target, fi os.FileInfo) error {
-	file, err := os.Open(fi.Name())
-	if err != nil {
-		return err
-	}
-
-	n := strings.Split(fi.Name(), ".")
-	fileType := mime.TypeByExtension("." + n[len(n)-1])
-
-	return target.bucket.PutReader(
-		target.path,
-		file,
-		fi.Size(),
-		fileType,
-		target.acl)
-}
-
-func multiSend(target Target, fi os.FileInfo) error {
-
-	var i int64
-
-	file, err := os.Open(fi.Name())
-	if err != nil {
-		return err
-	}
-
-	var mult s3.Multi
-	chans := make([]chan s3.Part, 0)
-
-	for i = 0; i < fi.Size(); i += ChunkSize {
-
-		chans = append(chans, make(chan s3.Part))
-
-		go func(mult s3.Multi, file *os.File, pos int64, ch chan<- s3.Part) {
-
-			reader := io.NewSectionReader(file, pos, ChunkSize)
-			part, err := mult.PutPart(int(pos/ChunkSize), reader)
-			if err != nil {
-				mult.Abort()
-			}
-			ch <- part
-			close(ch)
-
-		}(mult, file, i, chans[len(chans)-1])
-	}
-
-	parts := make([]s3.Part, 0)
-
-	for _, ch := range chans {
-		p := <-ch
-		parts = append(parts, p)
-	}
-
-	return mult.Complete(parts)
-
 }
 
 func finder(dir string, ch chan<- os.FileInfo) {
@@ -112,6 +67,8 @@ func finder(dir string, ch chan<- os.FileInfo) {
 		return
 	}
 
+	log.Println("Found", len(files), "files")
+
 	for _, f := range files {
 		ch <- f
 	}
@@ -126,9 +83,7 @@ func main() {
 
 	dir := flag.String("d", ".", "Directory to upload")
 	bucketName := flag.String("b", "", "Bucket name")
-	flag.StringVar(&target.path, "p", "", "Bucket path")
 	acl := flag.String("a", "private", "S3 ACL")
-	regionName := flag.String("r", "", "Region")
 	concurrency := flag.Int("c", 1, "Concurrency")
 
 	flag.Parse()
@@ -140,24 +95,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	region := aws.Region{Name: *regionName}
+	region := aws.EUWest
 	conn := s3.New(creds, region)
 
 	target.bucket = conn.Bucket(*bucketName)
 
 	ch := make(chan os.FileInfo)
 
-	finder(*dir, ch)
+	go finder(*dir, ch)
 
 	var wg sync.WaitGroup
 
 	for i := 0; i < *concurrency; i++ {
 		wg.Add(1)
 
-		go func(target Target, ch chan os.FileInfo) {
+		go func(target Target, ch chan os.FileInfo, dir string) {
 			defer wg.Done()
-			sendFiles(target, ch)
-		}(target, ch)
+			sendFiles(target, ch, dir)
+		}(target, ch, *dir)
 	}
 
 	wg.Wait()
